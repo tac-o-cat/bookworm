85edd69387a83061ae41bd7903330ebd9c224a8c
Minhee Kim <mini.kim@woowahan.com>
2021-08-30 00:09:37 +0900

    :tada: set electron

---
+++ src/main/main.ts
@@ -0,0 +1,59 @@
+import { app, BrowserWindow } from "electron";
+import * as isDev from "electron-is-dev";
+import * as path from "path";
+import * as url from "url";
+
+let mainWindow: BrowserWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    center: true,
+    kiosk: !isDev,
+    resizable: true,
+    fullscreen: false,
+    fullscreenable: true,
+    webPreferences: {
+      //   nodeIntegration: true,
+      //   enableRemoteModule: true,
+      devTools: isDev
+    }
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "./index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools({ mode: "detach" });
+  }
+
+  mainWindow.setResizable(true);
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => (mainWindow = undefined!));
+  mainWindow.focus();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const lodash = require("lodash");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const distDir = path.resolve(__dirname, "dist");
-const port = 4000;
-console.log(distDir);
-module.exports = {
+
+const commonConfig = {
   mode: "development",
   devtool: "inline-source-map",
-  entry: "./src/index.tsx",
   output: {
-    path: distDir,
-    filename: "bundle.[name].js"
+    path: distDir
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"]
@@ -21,11 +20,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ]
+        loader: "ts-loader"
       },
       {
         enforce: "pre",
@@ -46,20 +41,47 @@ module.exports = {
       { test: /\.css$/, use: ["css-loader", "style-loader"] }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ["dist"] })
-  ],
-  devServer: {
-    host: "localhost",
-    port: port,
-    open: true,
-    static: "./dist"
-  },
+  plugins: [new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ["dist"] })],
   externals: {
     react: "React",
     "react-dom": "ReactDOM"
   }
 };
+
+const mainConfig = lodash.cloneDeep(commonConfig);
+mainConfig.entry = "./src/main/main.ts";
+mainConfig.target = "electron-main";
+mainConfig.output.filename = "main.bundle.js";
+mainConfig.plugins = [
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: "package.json",
+        to: "package.json",
+        transform: (content) => {
+          const jsonContent = JSON.parse(content);
+          delete jsonContent.devDependencies;
+          delete jsonContent.scripts;
+          delete jsonContent.build;
+
+          jsonContent.main = "./main.bundle.js";
+          jsonContent.scripts = { start: "electron ./main.bundle.js" };
+          jsonContent.postinstall = "electron-builder install-app-deps";
+          return JSON.stringify(jsonContent, undefined, 2);
+        }
+      }
+    ]
+  })
+];
+
+const rendererConfig = lodash.cloneDeep(commonConfig);
+rendererConfig.entry = "./src/renderer/index.tsx";
+rendererConfig.target = "electron-renderer";
+rendererConfig.output.filename = "renderer.bundle.js";
+rendererConfig.plugins = [
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "./public/index.html")
+  })
+];
+
+module.exports = [mainConfig, rendererConfig];
