e4f7db0c61ebf5e8d05828f99cb84d584d068e9e
Minhee Kim <mini.kim@woowahan.com>
2021-09-06 20:54:16 +0900

    :sparkles: add react router dom

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { ListPage } from "./List/ListPage";
-
+console.log("??????????");
 export const App = () => (
-  <>
-    <ListPage />
-  </>
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Book shelve</Link>
+          </li>
+          <li>
+            <Link to="add">Add a book</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <Switch>
+      <Route exact path="/">
+        <ListPage />
+      </Route>
+      <Route exact path="/add">
+        <div>짜잔~</div>
+      </Route>
+    </Switch>
+  </Router>
 );
